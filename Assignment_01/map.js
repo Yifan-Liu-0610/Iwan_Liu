686da29b8b4804d3ea703b2e30f456eb237a7295
Yifan-Liu-0610 <yl3878@cornell.edu>
2025-02-05 19:02:22 -0500

    Add files via upload

---
+++ Assignment_01/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken ="pk.eyJ1IjoiaXdhbml3YW5saXUiLCJhIjoiY202ampscGt2MDFsMjJqb2o2cGJjeW04OCJ9.xNgm_C4PfUixfEfQKSCbHA";
+const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/iwaniwanliu/cm6p9kybr002z01qr29j68l7e",
+    zoom: 10,
+    center: [-74, 40.725]
+});
